e4df4ec9bec299f7659c3b6e9f78375b620941dc
fahpratan <33821437+fahpratan@users.noreply.github.com>
2017-12-27 11:47:52 +0700

    Add files via upload

---
+++ bot_settings.php
@@ -0,0 +1,6 @@
+<?php
+/// การตั้งค่าเกี่ยวกับ bot ใน LINE Messaging API
+define('LINE_MESSAGE_CHANNEL_ID','1549479354');
+define('LINE_MESSAGE_CHANNEL_SECRET','d2d4f67038314511e38ef2952dbf6e66');
+define('LINE_MESSAGE_ACCESS_TOKEN','S8TzqHkbXbka8fuEOcKWikC0QAIq8PrEuScRVHFTwaf/zZ/N/snNi+uUc2m7d4ygavhrJwyRY/GJbQYuJScE3LsfWpufj0Xvgp1NpRj2T6EMhcgAouT/OP9fCksN+alaqF1k+AcwhLNGsnOxyDumNAdB04t89/1O/w1cDnyilFU=');
+?>

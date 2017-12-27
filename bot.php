eafc2896cd8bf6a45ac15fe07a9422df3af133b6
fahpratan <33821437+fahpratan@users.noreply.github.com>
2017-12-27 17:06:24 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -190,7 +190,7 @@ if(!is_null($events)){
                         // กำหนด action 4 ปุ่ม 4 ประเภท
                         $actionBuilder = array(
                             new MessageTemplateActionBuilder(
-                                'Message Template',// ข้อความแสดงในปุ่ม
+                                'UTL1',// ข้อความแสดงในปุ่ม
                                 'This is Text' // ข้อความที่จะแสดงฝั่งผู้ใช้ เมื่อคลิกเลือก
                             ),
                             new UriTemplateActionBuilder(

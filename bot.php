aa4ce466f0be5e43f356307c72d54539c296e508
fahpratan <33821437+fahpratan@users.noreply.github.com>
2017-12-27 18:47:30 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -283,7 +283,7 @@ if(!is_null($events)){
                             )
                         );
                         break;      
-                      case "t_c":
+                      case "help":
                         // กำหนด action 4 ปุ่ม 4 ประเภท
                         $actionBuilder = array(
                             new MessageTemplateActionBuilder(

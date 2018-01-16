348defb5f6a9314a1f86303ab566618d7d695bd3
fahpratan <33821437+fahpratan@users.noreply.github.com>
2018-01-16 12:16:14 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -124,9 +124,7 @@ if(!is_null($events)){
                         $multiMessage1->add($replyData3);
                         $replyData = $multiMessage1;
                         break;
-                  for (($i=1;$i<52;$i++))
-                  do
-                  case $obj1[$i][1]: 
+                  case $obj1[2][1]: 
                         for ($i=1;$i<52;$i++){
                          ////
                          if($obj1[2][1] == $obj1[$i][1]){

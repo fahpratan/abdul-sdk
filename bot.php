8c2c2577e5b53822feed883bb92671a200f9ae79
fahpratan <33821437+fahpratan@users.noreply.github.com>
2018-01-16 10:00:42 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -124,12 +124,19 @@ if(!is_null($events)){
                         $multiMessage1->add($replyData3);
                         $replyData = $multiMessage1;
                         break;
+                  for ($i=1;$i<52;$i++){      
                   case $obj1[1][1]:     
-                        for ($i=1;$i<52;$i++){
                         $textReplyMessage = "E/N:".$obj1[$i][0]." "."NAME:".$obj1[$i][1]." ".$obj1[$i][2]." "."Nickname:".$obj1[$i][3]." "."ExtNo:".$obj1[$i][4];
                         $replyData = new TextMessageBuilder($textReplyMessage);
-                        }
-                        break;
+                        $check =1;
+                  if ($check==1){break;}
+                    } 
+                  //case $obj1[1][1]:     
+                  //      for ($i=1;$i<52;$i++){
+                  //      $textReplyMessage = "E/N:".$obj1[$i][0]." "."NAME:".$obj1[$i][1]." ".$obj1[$i][2]." "."Nickname:".$obj1[$i][3]." "."ExtNo:".$obj1[$i][4];
+                  //      $replyData = new TextMessageBuilder($textReplyMessage);
+                 //       }
+                  //      break;
                  //case "PRASAN":     
                        // for ($i=1;$i<52;$i++){
                        // $textReplyMessage = "E/N:".$obj1[$i][0]." "."NAME:".$obj1[$i][1]." ".$obj1[$i][2]." "."Nickname:".$obj1[$i][3]." "."ExtNo:".$obj1[$i][4];

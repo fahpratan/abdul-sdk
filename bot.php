39b3ec617d63124c5ce115461c868b17e3e9a646
fahpratan <33821437+fahpratan@users.noreply.github.com>
2018-01-17 14:02:52 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -144,71 +144,7 @@ if(!is_null($events)){
                        $check =1; 
                        }
 ///////////////////////////////////////////ASK PRINTER///////////////////////////////////////////////                    
-                  if(strtoupper($userMessage) == "PRINTER"){
-                        // กำหนด action 4 ปุ่ม 4 ประเภท
-                        $actionBuilder = array(
-                            new MessageTemplateActionBuilder(
-                                'UTL1',// ข้อความแสดงในปุ่ม
-                                'PRINTER1' // ข้อความที่จะแสดงฝั่งผู้ใช้ เมื่อคลิกเลือก
-                            ),
-                            new MessageTemplateActionBuilder(
-                                'UTL2',// ข้อความแสดงในปุ่ม
-                                'PRINTER2' // ข้อความที่จะแสดงฝั่งผู้ใช้ เมื่อคลิกเลือก
-                            ),
-                            new MessageTemplateActionBuilder(
-                                'UTL3',// ข้อความแสดงในปุ่ม
-                                'PRINTER3' // ข้อความที่จะแสดงฝั่งผู้ใช้ เมื่อคลิกเลือก
-                            ),
-    
-                        );
-                        $imageUrl = 'https://thetomatos.com/wp-content/uploads/2016/02/printer-clipart-5.png';
-                        $replyData = new TemplateMessageBuilder('Button Template',
-                            new ButtonTemplateBuilder(
-                                    'IP Printer UTL', // กำหนดหัวเรื่อง
-                                    'Please select UTL', // กำหนดรายละเอียด
-                                    $imageUrl, // กำหนด url รุปภาพ
-                                    $actionBuilder  // กำหนด action object
-                            )
-                        );              
-                   $check =1; 
-                  }
-                  if(strtoupper($userMessage) == "PRINTER1") {
-                        $picFullSize1 = 'https://raw.githubusercontent.com/fahpratan/abdul-sdk/master/ip-printer-utl1.JPG';
-                        $picThumbnail1 = 'https://raw.githubusercontent.com/fahpratan/abdul-sdk/master/ip-printer-utl1.JPG/240';
-                        $replyData1 = new ImageMessageBuilder($picFullSize1,$picThumbnail1);
-                        $picFullSize2 = 'https://raw.githubusercontent.com/fahpratan/abdul-sdk/master/ip-printer-utl1-2.JPG';
-                        $picThumbnail2 = 'https://raw.githubusercontent.com/fahpratan/abdul-sdk/master/ip-printer-utl1-2.JPG/240';
-                        $replyData2 = new ImageMessageBuilder($picFullSize2,$picThumbnail2);
-                        $picFullSize3 = 'https://raw.githubusercontent.com/fahpratan/abdul-sdk/master/ip-printer-utl1-3.JPG';
-                        $picThumbnail3 = 'https://raw.githubusercontent.com/fahpratan/abdul-sdk/master/ip-printer-utl1-3.JPG/240';
-                        $replyData3 = new ImageMessageBuilder($picFullSize3,$picThumbnail3);
-                       
-                        $multiMessage1 = new MultiMessageBuilder;
-                        $multiMessage1->add($replyData1);
-                        $multiMessage1->add($replyData2);
-                        $multiMessage1->add($replyData3);
-                        $replyData = $multiMessage1;  
-                        $check =1;
-                        }
-                  if(strtoupper($userMessage) == "PRINTER2") {
-                        $picFullSize2_1 = 'https://raw.githubusercontent.com/fahpratan/abdul-sdk/master/ip-printer-utl2-1.JPG';
-                        $picThumbnail2_1 = 'https://raw.githubusercontent.com/fahpratan/abdul-sdk/master/ip-printer-utl2-1.JPG/240';
-                        $replyData = new ImageMessageBuilder($picFullSize2_1,$picThumbnail2_1);
-                        $check =1;
-                        }
-                 if(strtoupper($userMessage) == "PRINTER3") {
-                        $picFullSize3_1 = 'https://raw.githubusercontent.com/fahpratan/abdul-sdk/master/ip-printer-utl3-1.JPG';
-                        $picThumbnail3_1 = 'https://raw.githubusercontent.com/fahpratan/abdul-sdk/master/ip-printer-utl3-1.JPG/240';
-                        $replyData3_1 = new ImageMessageBuilder($picFullSize3_1,$picThumbnail3_1);
-                        $picFullSize3_2 = 'https://raw.githubusercontent.com/fahpratan/abdul-sdk/master/ip-printer-utl3-2.JPG';
-                        $picThumbnail3_2 = 'https://raw.githubusercontent.com/fahpratan/abdul-sdk/master/ip-printer-utl3-2.JPG/240';
-                        $replyData3_2 = new ImageMessageBuilder($picFullSize3_2,$picThumbnail3_2);
-                        $multiMessage3 = new MultiMessageBuilder;
-                        $multiMessage3->add($replyData3_1);
-                        $multiMessage3->add($replyData3_2);
-                        $replyData = $multiMessage3;
-                        $check =1;
-                        }  
+include 'Ask_Printer.php';
 ///////////////////////////////////////////////////////////////////MAP//////////////////////////////////////////////// 
                  if(strtoupper($userMessage) == "MAP") {
                         // กำหนด action 4 ปุ่ม 4 ประเภท

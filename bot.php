fffcf4a66d3bf4b7cf0c04183f5d5f418ca92968
fahpratan <33821437+fahpratan@users.noreply.github.com>
2018-04-11 16:11:48 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -118,8 +118,8 @@ if(!is_null($events)){
          /////////////////////////////////
          for ($i=1;$i<5967;$i++){
                   if($userMessage == $obj2[$i][1]){
-                    $textReplyMessage = "E/N:".$obj2[$i][1]." "."NAME:".$obj2[$i][2]." ".$obj2[$i][3]." "."GROUP:".$obj2[$i][4]." "."DEPT:".$obj2[$i][5]." "."SUP:".$obj2[$i][6]." "."TEL:".$obj2[$i][7]." "."TYPE:".$obj2[$i][8];
-                    $replyData = new TextMessageBuilder($textReplyMessage);
+                    $textReplyMessage[$i] = "E/N:".$obj2[$i][1]." "."NAME:".$obj2[$i][2]." ".$obj2[$i][3]." "."GROUP:".$obj2[$i][4]." "."DEPT:".$obj2[$i][5]." "."SUP:".$obj2[$i][6]." "."TEL:".$obj2[$i][7]." "."TYPE:".$obj2[$i][8];
+                    $replyData = new TextMessageBuilder($textReplyMessage[$i]);
                     $check =1;
                     }
                     if ($check==1){break;}

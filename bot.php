d18c4bb0b184ac918039f4e93c1bdf07c8be1906
fahpratan <33821437+fahpratan@users.noreply.github.com>
2018-04-11 16:27:00 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -56,7 +56,6 @@ $bot = new LINEBot($httpClient, array('channelSecret' => LINE_MESSAGE_CHANNEL_SE
 $content = file_get_contents('php://input');
 // แปลงข้อความรูปแบบ JSON  ให้อยู่ในโครงสร้างตัวแปร array
 $events = json_decode($content, true);
-
 //DATA TELL UTL
 $obj2 = array();
 $Name_tel_UTL_1 = fopen('filesort.csv', 'r');
@@ -106,20 +105,73 @@ if(!is_null($events)){
     if(!is_null($is_message)){
         if($typeMessage ='text'){
 ///////////////////////////////////////ASK TEL UTL////////////////////
-      $Name_tel_UTL = fopen('filesort.csv', 'r');
+    //              $check = 0;
+    //              for ($i=1;$i<5967;$i++){
+    //              if(strtoupper($userMessage) == $obj2[$i][2]){
+                    //while(strtoupper($userMessage) == $obj2[$i][2]){
+     
+    //                    $textReplyMessage1 = "E/N:".$obj2[$i][1]." "."NAME:".$obj2[$i][2]." ".$obj2[$i][3]." "."GROUP:".$obj2[$i][4]." "."DEPT:".$obj2[$i][5]." "."SUP:".$obj2[$i][6]." "."TEL:".$obj2[$i][7]." "."TYPE:".$obj2[$i][8];
+    //                    $textMessage1 = new TextMessageBuilder($textReplyMessage1);
+                                         
+    //                    $textReplyMessage2 = "E/N:".$obj2[$i][1]." "."NAME:".$obj2[$i][2]." ".$obj2[$i][3]." "."GROUP:".$obj2[$i][4]." "."DEPT:".$obj2[$i][5]." "."SUP:".$obj2[$i][6]." "."TEL:".$obj2[$i][7]." "."TYPE:".$obj2[$i][8];
+    //                    $textMessage2 = new TextMessageBuilder($textReplyMessage2);
+                                         
+    //                    $textReplyMessage3 = "E/N:".$obj2[$i][1]." "."NAME:".$obj2[$i][2]." ".$obj2[$i][3]." "."GROUP:".$obj2[$i][4]." "."DEPT:".$obj2[$i][5]." "."SUP:".$obj2[$i][6]." "."TEL:".$obj2[$i][7]." "."TYPE:".$obj2[$i][8];
+    //                    $textMessage3 = new TextMessageBuilder($textReplyMessage3);                  
+                        
+    //                    $textReplyMessage4 = "E/N:".$obj2[$i][1]." "."NAME:".$obj2[$i][2]." ".$obj2[$i][3]." "."GROUP:".$obj2[$i][4]." "."DEPT:".$obj2[$i][5]." "."SUP:".$obj2[$i][6]." "."TEL:".$obj2[$i][7]." "."TYPE:".$obj2[$i][8];
+    //                    $textMessage4 = new TextMessageBuilder($textReplyMessage4);
+                        
+    //                    $textReplyMessage5 = "E/N:".$obj2[$i][1]." "."NAME:".$obj2[$i][2]." ".$obj2[$i][3]." "."GROUP:".$obj2[$i][4]." "."DEPT:".$obj2[$i][5]." "."SUP:".$obj2[$i][6]." "."TEL:".$obj2[$i][7]." "."TYPE:".$obj2[$i][8];
+    //                    $textMessage5 = new TextMessageBuilder($textReplyMessage5);
+    
+    //                    $multiMessage = new MultiMessageBuilder;
+    //                    $multiMessage->add($textMessage1);
+    //                    $multiMessage->add($textMessage2);
+    //                    $multiMessage->add($textMessage3);
+    //                    $multiMessage->add($textMessage4);
+    //                    $multiMessage->add($textMessage5);
+    //                    $replyData = $multiMessage; 
+    //                    $check =1;   
+    //                }
+                   // }
+    //                if ($check==1){break;}
+    //
+        $Name_tel_UTL = fopen('filesort.csv', 'r');
     	while (($objArr = fgetcsv($Name_tel_UTL)) !== FALSE) {
-	if(strtoupper($userMessage)==$objArr[2]){
-	     $textReplyMessage = "E/N:".$objArr[1]." "."NAME:".$objArr[2]." ".$objArr[3]." "."GROUP:".$objArr[4]." "."DEPT:".$objArr[5]." "."SUP:".$objArr[6]." "."TEL:".$objArr[7]." "."TYPE:".$objArr[8];
-             $replyData = new TextMessageBuilder($textReplyMessage);
-	     $check =1;
-			}
-             // if ($check==1){break;}                
+	    if(trim(strtoupper($userMessage))==$objArr[2]){
+	    // $textReplyMessage = "E/N:".$objArr[1]." "."NAME:".$objArr[2]." ".$objArr[3]." "."GROUP:".$objArr[4]." "."DEPT:".$objArr[5]." "."SUP:".$objArr[6]." "."TEL:".$objArr[7]." "."TYPE:".$objArr[8];
+        //     $replyData = new TextMessageBuilder($textReplyMessage);
+	    //$check =1;
+		        $textReplyMessage1 = "E/N:".$obj2[$i][1]." "."NAME:".$obj2[$i][2]." ".$obj2[$i][3]." "."GROUP:".$obj2[$i][4]." "."DEPT:".$obj2[$i][5]." "."SUP:".$obj2[$i][6]." "."TEL:".$obj2[$i][7]." "."TYPE:".$obj2[$i][8];
+                $textMessage1 = new TextMessageBuilder($textReplyMessage1);
+                $textReplyMessage2 = "E/N:".$obj2[$i][1]." "."NAME:".$obj2[$i][2]." ".$obj2[$i][3]." "."GROUP:".$obj2[$i][4]." "."DEPT:".$obj2[$i][5]." "."SUP:".$obj2[$i][6]." "."TEL:".$obj2[$i][7]." "."TYPE:".$obj2[$i][8];
+				$textMessage2 = new TextMessageBuilder($textReplyMessage2);
+				$textReplyMessage3 = "E/N:".$obj2[$i][1]." "."NAME:".$obj2[$i][2]." ".$obj2[$i][3]." "."GROUP:".$obj2[$i][4]." "."DEPT:".$obj2[$i][5]." "."SUP:".$obj2[$i][6]." "."TEL:".$obj2[$i][7]." "."TYPE:".$obj2[$i][8];
+				$textMessage3 = new TextMessageBuilder($textReplyMessage3);                  
+				$textReplyMessage4 = "E/N:".$obj2[$i][1]." "."NAME:".$obj2[$i][2]." ".$obj2[$i][3]." "."GROUP:".$obj2[$i][4]." "."DEPT:".$obj2[$i][5]." "."SUP:".$obj2[$i][6]." "."TEL:".$obj2[$i][7]." "."TYPE:".$obj2[$i][8];
+				$textMessage4 = new TextMessageBuilder($textReplyMessage4);
+				$textReplyMessage5 = "E/N:".$obj2[$i][1]." "."NAME:".$obj2[$i][2]." ".$obj2[$i][3]." "."GROUP:".$obj2[$i][4]." "."DEPT:".$obj2[$i][5]." "."SUP:".$obj2[$i][6]." "."TEL:".$obj2[$i][7]." "."TYPE:".$obj2[$i][8];
+				$textMessage5 = new TextMessageBuilder($textReplyMessage5);
+				$multiMessage = new MultiMessageBuilder;
+				$multiMessage->add($textMessage1);
+				$multiMessage->add($textMessage2);
+				$multiMessage->add($textMessage3);
+				$multiMessage->add($textMessage4);
+				$multiMessage->add($textMessage5);
+				$replyData = $multiMessage; 
+				$check =1;   
+				}
+				 }
+				if ($check==1){break;}
+							}
+              if ($check==1){break;}                
               }
          /////////////////////////////////
          for ($i=1;$i<5967;$i++){
                   if($userMessage == $obj2[$i][1]){
-                    $textReplyMessage[$i] = "E/N:".$obj2[$i][1]." "."NAME:".$obj2[$i][2]." ".$obj2[$i][3]." "."GROUP:".$obj2[$i][4]." "."DEPT:".$obj2[$i][5]." "."SUP:".$obj2[$i][6]." "."TEL:".$obj2[$i][7]." "."TYPE:".$obj2[$i][8];
-                    $replyData = new TextMessageBuilder($textReplyMessage[$i]);
+                    $textReplyMessage = "E/N:".$obj2[$i][1]." "."NAME:".$obj2[$i][2]." ".$obj2[$i][3]." "."GROUP:".$obj2[$i][4]." "."DEPT:".$obj2[$i][5]." "."SUP:".$obj2[$i][6]." "."TEL:".$obj2[$i][7]." "."TYPE:".$obj2[$i][8];
+                    $replyData = new TextMessageBuilder($textReplyMessage);
                     $check =1;
                     }
                     if ($check==1){break;}
@@ -274,7 +326,6 @@ if(!is_null($events)){
                         $picFullSizeMAP1 = 'https://raw.githubusercontent.com/fahpratan/abdul-sdk/master/UTL1-MAP.jpg';
                         $picThumbnailMAP1 = 'https://raw.githubusercontent.com/fahpratan/abdul-sdk/master/UTL1-MAP.jpg/240';
                         $imageMessage = new ImageMessageBuilder($picFullSizeMAP1,$picThumbnailMAP1);
-
                         $multiMessage = new MultiMessageBuilder;                        
                         $multiMessage->add($locationMessage);
                         $multiMessage->add($imageMessage);
@@ -291,7 +342,6 @@ if(!is_null($events)){
                         $picFullSizeMAP2 = 'https://raw.githubusercontent.com/fahpratan/abdul-sdk/master/UTL2-MAP.jpg';
                         $picThumbnailMAP2 = 'https://raw.githubusercontent.com/fahpratan/abdul-sdk/master/UTL2-MAP.jpg/240';
                         $imageMessage = new ImageMessageBuilder($picFullSizeMAP2,$picThumbnailMAP2);
-
                         $multiMessage = new MultiMessageBuilder;                        
                         $multiMessage->add($locationMessage);
                         $multiMessage->add($imageMessage);
@@ -308,7 +358,6 @@ if(!is_null($events)){
                         $picFullSizeMAP3 = 'https://raw.githubusercontent.com/fahpratan/abdul-sdk/master/UTL3-MAP.jpg';
                         $picThumbnailMAP3 = 'https://raw.githubusercontent.com/fahpratan/abdul-sdk/master/UTL3-MAP.jpg/240';
                         $imageMessage = new ImageMessageBuilder($picFullSizeMAP3,$picThumbnailMAP3);
-
                         $multiMessage = new MultiMessageBuilder;                        
                         $multiMessage->add($locationMessage);
                         $multiMessage->add($imageMessage);

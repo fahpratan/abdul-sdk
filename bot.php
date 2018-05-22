e330e53b62a45b2b7b89127ccb9f7d3d10983c83
fahpratan <33821437+fahpratan@users.noreply.github.com>
2018-05-22 18:01:54 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -140,7 +140,7 @@ if(!is_null($events)){
                        }
 //////////////////////////////////////////////////////////////////////////////////////////////////////
 		   if(strtoupper($userMessage) == "F"){
-                        $textReplyMessage = "https://github.com/fahpratan/abdul-sdk/blob/master/filesort.csv";
+                        $textReplyMessage = "https://raw.githubusercontent.com/fahpratan/abdul-sdk/master/filesort.csv";
                     	$replyData = new TextMessageBuilder($textReplyMessage);
                         $check =1; 
                        }
